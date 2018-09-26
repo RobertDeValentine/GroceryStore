ad8f28a813ba01c2cf1f22f7bbc420de7f188047
RobertDeValentine <rjdevalentine@mail.csuchico.edu>
2018-09-26 16:52:08 -0700

    in addition to previous on entering an a will check to see if there are lees than 6 items in array and then add item entered to array orgive error statement if array is already full

--- main.cpp
+++ main.cpp
@@ -6,6 +6,7 @@ using namespace std;
 int main()
 {
   string list[5]; //array of 5 strings
+  string item; //string of item to be put in array
   int numItems = 0;
   char input;
 
@@ -17,8 +18,22 @@ int main()
      cout<<"\n (Q)uit";
      cout<<"\nYour choice (A/Q): ";
      cin>>input;
-
-
+     
+     // captures string and puts in array
+     if (input=='a'||input=='A')
+     { 
+        cout<<"What is the item?\n";
+        cin>>item;
+        if (numItems<=4)
+        {
+             list[numItems] = item;
+             numItems++;
+        }
+        else if (numItems >=5)
+        {
+             cout<<"You'll need a bigger list!\n";
+        }
+     }
 
   }while( !(input=='q'||input=='Q') );
 
