d972fc4fcb4c6435a92e92f0c944615c08191f3a
RobertDeValentine <rjdevalentine@mail.csuchico.edu>
2018-10-01 16:22:19 -0700

    now prints out the list when quiting

--- main.cpp
+++ main.cpp
@@ -36,7 +36,11 @@ int main()
      }
 
   }while( !(input=='q'||input=='Q') );
+cout<<"==ITEMS TO BUY==\n";
 
-
+for(int index = 0; index<5 ;index++)
+{
+cout<<index + 1<<" "<<list[index]<<endl;
+}
   return 0;
 }
