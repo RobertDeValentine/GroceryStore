86b26cc66052910290244d53192fbce815c89df9
RobertDeValentine <rjdevalentine@mail.csuchico.edu>
2018-10-10 16:40:52 -0700

    Will now subtract last item from list if entering the correct prompt

--- main.cpp
+++ main.cpp
@@ -16,6 +16,7 @@ int main()
      cout<<"\n==GROCERY LIST MANAGER==";
      cout<<"\nEnter your choice: ";
      cout<<"\n (A)dd an item";
+     cout<<"\n (D)elete last item";
      cout<<"\n (Q)uit";
      cout<<"\nYour choice (A/Q): ";
      cin>>input;
@@ -26,7 +27,11 @@ int main()
         cout<<"What is the item?\n";
         cin>>item;
         list.push_back(item);
-        numItems++;
+     }
+     else if ((input =='d' || input=='D')&& list.size()!=0)
+     {
+        cout<<list[list.size()-1]<<" was deleted from the list.\n";
+        list.pop_back();
      }
 
   }while( !(input=='q'||input=='Q') );
